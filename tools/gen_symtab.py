aad594ccd383b41dc1edab4e6bf5a40f32a3f918
Agustina Arzille <avarzille@riseup.net>
2022-06-11 14:54:59 -0300

    Fix style in symbol table generator

--- tools/gen_symtab.py
+++ tools/gen_symtab.py
@@ -7,38 +7,40 @@ import sys
 
 symtab = []
 
-def valid_symbol(parts):
-  if len(parts) != 4:
+def valid_symbol (parts):
+  if len (parts) != 4:
     return False
 
   sym = parts[3]
-  if (sym.startswith("__func__") or sym.startswith("CSWTCH") or
-      (parts[2] == 'r' and sym.startswith("symbol_name_"))):
+  if (sym.startswith ("__func__") or sym.startswith ("CSWTCH") or
+      (parts[2] == 'r' and sym.startswith ("symbol_name_"))):
     return False
 
   del parts[2]
   return True
 
 for line in sys.stdin:
-  line = line.strip()
-  parts = line.split(' ')
+  line = line.strip ()
+  parts = line.split (' ')
 
-  if not valid_symbol(parts):
+  if not valid_symbol (parts):
     continue
 
   sym = {'addr': parts[0], 'size': parts[1], 'name': parts[2]}
-  symtab.append(sym)
+  symtab.append (sym)
 
-print("#include <kern/symbol.h>")
+print ("#include <kern/symbol.h>")
 
-for index, sym in enumerate(symtab):
-  print("static const char symbol_name_%u[] __symbol_table = \"%s\";" % (index, sym['name']))
+for index, sym in enumerate (symtab):
+  print ("static const char symbol_name_%u[] __symbol_table = \"%s\";" %
+    (index, sym['name']))
 
-print("const struct symbol symbol_table[] __symbol_table = {")
+print ("const struct symbol symbol_table[] __symbol_table = {")
 
-for index, sym in enumerate(symtab):
-  print("    { 0x%s, 0x%s, symbol_name_%u }," % (sym['addr'], sym['size'], index))
+for index, sym in enumerate (symtab):
+  print ("    { 0x%s, 0x%s, symbol_name_%u }," %
+    (sym['addr'], sym['size'], index))
 
-print("};")
-print("const size_t symbol_table_size = %d;" % len(symtab))
-print("const struct symbol *symbol_table_ptr = symbol_table;")
+print ("};")
+print ("const size_t symbol_table_size = %d;" % len (symtab))
+print ("const struct symbol *symbol_table_ptr = symbol_table;")
