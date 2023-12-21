17fb270dd3de1f7ccbf57bf77d43ede39e0f6402
Agustina Arzille <avarzille@riseup.net>
2023-12-21 16:32:55 -0300

    Also test interrupt alert type

--- test/test_intr.c
+++ test/test_intr.c
@@ -85,6 +85,7 @@ test_intr (void *arg)
   error = cap_recv_alert (flow, &data->alert, sizeof (data->alert), &data->md);
   assert (! error);
 
+  assert (data->alert.type == CAP_ALERT_INTR);
   assert (data->alert.intr.irq == TEST_INTR_FIRST);
   assert (data->alert.intr.count == 2);
   assert (data->md.task_id == 0);
@@ -99,6 +100,7 @@ test_intr (void *arg)
   error = cap_recv_alert (flow, &data->alert, sizeof (data->alert), &data->md);
   assert (! error);
 
+  assert (data->alert.type == CAP_ALERT_INTR);
   assert (data->alert.intr.irq == TEST_INTR_FIRST);
   assert (data->alert.intr.count == 1);
   assert (data->md.task_id == 0);
